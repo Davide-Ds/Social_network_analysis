2e63e2c59cee4618c2b20455f114b5f281ff19ad
Davide De Simone <desimonedavide97@gmail.com>
2025-03-05 01:05:25 +0100

    refactor anomaly detection logic in find_anomalies.py and update README with example clarifications

--- find_anomalies.py
+++ find_anomalies.py
@@ -12,6 +12,7 @@ tweet_relations = defaultdict(list)
 # Funzione per estrarre le tuple dai file
 def parse_tree_file(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
+        next(f)  # Skip the first line ['ROOT', 'ROOT', '0.0']
         lines = f.readlines()
     
     edges = []
@@ -31,7 +32,7 @@ def find_anomalies():
     anomalies = []
     i = 0
     for filename in os.listdir(data_dir):
-      if i < 3:
+      if i < 1:
         i+= 1
         if filename.endswith(".txt"):
             filepath = os.path.join(data_dir, filename)
@@ -45,9 +46,10 @@ def find_anomalies():
                 if parent_time > child_time:
                     anomalies.append((filename, line, "Child before parent"))
                 if len(set(t[0] for t in tweet_relations[child_tid])) > 1:
+                    print(set(t[0] for t in tweet_relations[child_tid]))
                     anomalies.append((filename, line, "Multiple inconsistent parents"))
-                if child_tid in tweet_relations and any(p[0] == child_tid for p in tweet_relations[child_tid]):
-                    anomalies.append((filename, line, "Tweet is both parent and child"))
+                """if child_tid in tweet_relations and any(p[0] == child_tid for p in tweet_relations[child_tid]):
+                    anomalies.append((filename, line, "Tweet is both parent and child"))"""
     
     return anomalies
 
